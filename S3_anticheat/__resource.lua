324db391877ba46cf85a6c87013554bfeb0a1c4b
Siebeke3 <89068843+Siebeke3@users.noreply.github.com>
2022-09-10 00:09:50 +0200

    Volledige custom anticheat ( Siebeke3)

---
+++ S3_anticheat/__resource.lua
@@ -0,0 +1,22 @@
+
+beschrijving 'S3_anticheat'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+    's3anticheat-cl.lua'
+}
+server_scripts {
+	'@es_extended/locale.lua',
+	'@async/async.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'locales/en.lua',
+	'config.lua',
+    's3anticheat-sv.lua'
+}
+
+dependencies {
+	'essentialmode',
+	'async'
+}
